75c1d738eb810fb96018cdb579eca12ed8ced69f
ArcyAs <55660878+ArcyAs@users.noreply.github.com>
2020-02-12 17:12:34 +0100

    poprawa

---
+++ PoprawaKolokwium2/PoprawaKolokwium2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PoprawaKolokwium2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
